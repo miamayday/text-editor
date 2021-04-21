bc9d6457418b76a0d93d35c7dd519e91bf5a2f33
Mia Mantila <mia.mantila@aalto.fi>
2021-04-21 13:49:55 +0300

    Add editor base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+type EditorProps = {}
+
+type EditorState = {
+  paragraphList: Array<string>
+}
+
+class Editor extends React.Component<EditorProps, EditorState> {
+  state: EditorState = {
+    paragraphList: [
+      'This is an example text.',
+      'Another one.',
+      'This example positions a "highlight" rectangle behind the contents of a range. The range\'s content starts here and continues on until it ends here. The bounding client rectangle contains everything selected in the range.',
+      'More text.',
+      'Should be scrolling soon.',
+      'The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.'
+    ]
+  }
+  render() {
+    return (
+      <div className="editor">
+        {this.state.paragraphList.map((p, i) => (
+          <p key={i}>{p}</p>
+        ))}
+      </div>
+    )
+  }
+}
+
+type MyProps = {}
+
+type MyState = {}
+
+class App extends React.Component<MyProps, MyState> {
+  state: MyState = {}
+  render() {
+    return (
+      <div className="app">
+        <Editor />
+      </div>
+    )
+  }
 }
 
 export default App
