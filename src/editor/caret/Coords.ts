e40d3c3269983c0adffd8dd22cf56766633e6688
Mia Mantila <mia.mantila@aalto.fi>
2021-11-26 21:04:59 +0200

    Add new example paragraph, continue cleaning Setter.ts

--- src/editor/caret/Coords.ts
+++ src/editor/caret/Coords.ts
@@ -11,6 +11,28 @@ export function getRectFromRange(node: Node, offset: number): DOMRect {
   return range.getBoundingClientRect()
 }
 
+export function getViewportCoords(
+  span: Element,
+  offset: number
+): [number, number] {
+  const rect = getRectFromRange(span.childNodes[0], offset)
+  const x = rect.left
+  const y = rect.top
+  return [x, y]
+}
+
+export function getDocumentCoords(
+  span: Element,
+  offset: number
+): [number, number] {
+  const d = document.querySelectorAll('.document')[0]
+  const cont = d.getBoundingClientRect()
+  let [x, y] = getViewportCoords(span, offset)
+  x = x - cont.left
+  y = y - cont.top + d.scrollTop
+  return [x, y]
+}
+
 export function getCoords(span: Element, offset: number): [number, number] {
   const rect = getRectFromRange(span.childNodes[0], offset)
   const d = document.querySelectorAll('.document')[0]
--- src/editor/caret/Setter.ts
+++ src/editor/caret/Setter.ts
@@ -3,162 +3,8 @@
    Horrible, horrible things happen here! */
 
 import * as Coords from './Coords'
-import { TextNode, Position, EditorState } from '../Types'
+import { Position, EditorState } from '../Types'
 import config from '../../config'
-
-// TODO: Clean up the code, look for possible issues
-
-/**
- * Fixes the caret to the nearest span element (text node) in the paragraph.
- *
- * @param p Paragaph element (div)
- * @param arr Array of paragraphs (text node arrays)
- * @param offset focusOffset
- * @param clickX Mouse click x position
- * @param clickY Mouse click y position
- * @returns New state
- */
-function fixToNearestSpan(
-  p: HTMLElement,
-  arr: Array<TextNode>,
-  offset: number,
-  clickX: number,
-  clickY: number
-): Position {
-  const pos: Position = {
-    caret: { offset, x: 0, y: 0 },
-    pindex: 0,
-    sindex: 0
-  }
-
-  const d = document.querySelectorAll('.document')[0]
-  const cont = d.getBoundingClientRect()
-
-  const outOfBounds = checkBounds(
-    p,
-    arr,
-    clickX,
-    cont.left + config.PARAGRAPH_PADDING
-  )
-
-  if (outOfBounds && offset > 0) {
-    console.log('snap to start')
-
-    for (let si = 0; si < arr.length; si++) {
-      const node = arr[si]
-      let nextOffset = offset + 1
-      let nextSindex = si
-      if (node.text.length < nextOffset) {
-        nextOffset = 0
-        nextSindex = si + 1
-      }
-
-      const span = p.children[nextSindex]
-      const rect = Coords.getRectFromRange(span.childNodes[0], nextOffset)
-      const y = rect.top
-
-      if (y <= clickY && clickY <= y + config.CARET_HEIGHT) {
-        // on the same line
-        pos.caret.x = config.PARAGRAPH_PADDING
-        pos.caret.y = rect.top - cont.top + d.scrollTop
-        pos.sindex = si
-        return pos
-      } else if (y > clickY) {
-        console.log('already past y')
-        // WRONG!!
-        break
-      }
-    }
-  }
-
-  let bestDiff = Number.MAX_VALUE
-  let bestSindex = 0
-
-  console.log('traverse spans containing offset')
-
-  // traverse the spans that contain the offset
-  for (let sindex = 0; sindex < arr.length; sindex++) {
-    const node = arr[sindex]
-    if (node.text.length === 0) {
-      // happens with empty paragraphs
-      console.log('empty paragraph')
-      const x = cont.left + config.PARAGRAPH_PADDING
-      const y = p.offsetTop + cont.top + d.scrollTop
-      const diff = Math.sqrt(Math.pow(clickX - x, 2) + Math.pow(clickY - y, 2))
-      if (diff < bestDiff) {
-        bestDiff = diff
-        bestSindex = 0
-        pos.caret.x = config.PARAGRAPH_PADDING
-        pos.caret.y = p.offsetTop + config.ADJUST_Y
-        pos.caret.offset = 0
-      }
-      break
-    } else if (node.text.length >= offset) {
-      const span = p.children[sindex]
-      const rect = Coords.getRectFromRange(span.childNodes[0], offset)
-      const [x, y] = [rect.left, rect.top]
-
-      // when surpass final bound just stop
-      if (y > clickY) {
-        const divider = y - config.ADJUST_Y
-        console.log('divider:', divider)
-        const finalBound = divider + config.LINE_HEIGHT
-        if (y > finalBound) {
-          // does it ever come to this??
-          console.log('surpass final bound')
-          break
-        }
-      }
-
-      const diff = Math.sqrt(Math.pow(clickX - x, 2) + Math.pow(clickY - y, 2))
-      if (diff < bestDiff) {
-        bestDiff = diff
-        bestSindex = sindex
-        pos.caret.x = rect.left - cont.left
-        pos.caret.y = rect.top - cont.top + d.scrollTop
-      }
-    }
-  }
-
-  pos.sindex = bestSindex
-  return pos
-}
-
-// TODO: Replace
-/**
- * Checks if the user clicked on the empty space to the left of a line.
- *
- * @param p Paragraph element (div)
- * @param arr Array of paragraphs (text node arrays)
- * @param clickX Mouse click x position
- * @param start X coordinate for left text boundary
- * @returns True if user clicked on the empty space
- */
-function checkBounds(
-  p: Element,
-  arr: Array<TextNode>,
-  clickX: number,
-  start: number
-): boolean {
-  if (arr[0].text.length >= 1) {
-    // check the span has room
-    const rect = Coords.getRectFromRange(p.children[0].childNodes[0], 1)
-    const next = rect.left
-    if (clickX < Coords.calcMiddle(start, next)) {
-      // check the mouse coords are out of bounds (the mid x of first char)
-      return true
-    }
-  } else if (arr.length > 1) {
-    // check the next span has room
-    const rect = Coords.getRectFromRange(p.children[1].childNodes[0], 0)
-    const next = rect.left
-    if (clickX < Coords.calcMiddle(start, next)) {
-      // check the mouse coords are out of bounds (the mid x of first char)
-      return true
-    }
-  }
-  return false
-}
 
 // TODO: Helper
 function incrementOffset(
@@ -176,7 +22,7 @@ function incrementOffset(
   if (offset <= editor.paragraphs[pindex][sindex].text.length) {
     return { offset, pindex, sindex }
   } else {
-    // go to next span
+    // Go to next span
     offset = 1
     sindex++
   }
@@ -184,7 +30,7 @@ function incrementOffset(
   if (sindex < editor.paragraphs[pindex].length) {
     return { offset, pindex, sindex }
   } else {
-    // go to next paragraph
+    // Go to next paragraph
     pindex++
     sindex = 0
     offset = 0
@@ -193,9 +39,46 @@ function incrementOffset(
   if (pindex < editor.paragraphs.length) {
     return { offset, pindex, sindex }
   } else {
-    // reach end of document
+    // Reach end of document
     return null
   }
+}
+
+function checkOffsetRepeat(
+  editor: EditorState,
+  pos: Position,
+  p: Element,
+  mouseX: number,
+  mouseY: number
+) {
+  const currSpan = p.children[pos.sindex]
+  const [currX, currY] = Coords.getDocumentCoords(currSpan, pos.caret.offset)
+  const currDist = Math.sqrt(
+    Math.pow(currX - mouseX, 2) + Math.pow(currY - mouseY, 2)
+  )
+
+  const nextPos = incrementOffset(editor, pos)
+  if (nextPos !== null) {
+    const nextSpan = p.children[nextPos.sindex]
+    let [nextX, nextY] = Coords.getDocumentCoords(nextSpan, nextPos.offset)
+
+    // Check if user clicked at the start by comparing the bounds
+    // of the current position and next position
+    if (currY !== nextY) {
+      nextX = config.PARAGRAPH_PADDING
+      const nextDist = Math.sqrt(
+        Math.pow(nextX - mouseX, 2) + Math.pow(nextY - mouseY, 2)
+      )
+      if (nextDist < currDist) {
+        pos.caret.x = nextX
+        pos.caret.y = nextY
+        return nextDist
+      }
+    }
+  }
+  pos.caret.x = currX
+  pos.caret.y = currY
+  return currDist
 }
 
 function calculateForSpan(
@@ -208,8 +91,15 @@ function calculateForSpan(
 ): void {
   console.log('Set caret for span:', el)
   console.log('focusOffset:', offset)
-  console.log('clientX:', clickX)
-  console.log('clientY:', clickY)
+
+  // The document which contains the paragraph elements
+  const d = document.querySelectorAll('.document')[0]
+  // The bounds of the document relative to the viewport
+  const cont = d.getBoundingClientRect()
+
+  // Mouse coordinates relative to the document
+  const mouseX = clickX - cont.left
+  const mouseY = clickY - cont.top + d.scrollTop
 
   // Consider a situation where the user clicks between
   // two consequtive spans. You get an element and an offset.
@@ -217,30 +107,17 @@ function calculateForSpan(
   // In this situation, the position should be set according
   // to the previous span.
 
-  console.log('\nCHECK TRANSITION\n\n')
-
   // Paragraph as an element
   const p = document.querySelectorAll('.paragraph')[pos.pindex]
-  // The bounds of the paragraph relative to the viewport
-  // * https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-  const bounds = p.getBoundingClientRect()
-  // Mouse x coordinate relative to the paragraph
-  const pX = clickX - bounds.left
 
-  console.log('Paragraph bounds:', bounds)
-  console.log('X coordinate within paragraph:', pX)
-  console.log('X coordinate within viewport:', clickX)
-
-  // Keep track of the shorter distance to cursor position
+  // Keep track of the shortest distance to cursor position
   let shortestDist = Number.MAX_VALUE
 
   if (offset <= editor.paragraphs[pos.pindex][pos.sindex].text.length) {
-    // Calculate caret position within the current span
-    ;[pos.caret.x, pos.caret.y] = Coords.getCoords(el, offset)
-    console.log('caret.x:', pos.caret.x)
-    // Calculate distance to the cursor position (relative to paragraph)
-    shortestDist = Math.abs(pos.caret.x - pX)
-    console.log('Distance to cursor position:', shortestDist)
+    // Calculate caret position relative to the document
+    ;[pos.caret.x, pos.caret.y] = Coords.getDocumentCoords(el, offset)
+    // Calculate distance to the cursor position
+    shortestDist = Math.abs(pos.caret.x - mouseX)
   }
 
   if (
@@ -249,12 +126,10 @@ function calculateForSpan(
   ) {
     if (el.previousSibling !== null) {
       const prevSpan = el.previousSibling as HTMLElement
-      // Calculate caret position within the previous span
-      const [prevX, prevY] = Coords.getCoords(prevSpan, offset)
-      console.log('prev.x:', prevX)
-      // Calculate distance to the cursor position (relative to paragraph)
-      const dist = Math.abs(prevX - pX)
-      console.log('Distance to cursor position:', dist)
+      // Calculate caret position in the previous span (relative to document)
+      const [prevX, prevY] = Coords.getDocumentCoords(prevSpan, offset)
+      // Calculate distance to the cursor position
+      const dist = Math.abs(prevX - mouseX)
       // Compare this to the current shortest distance.
       // If the values are the same, favor the previous span.
       // This is because it is assumed that the caret position
@@ -284,43 +159,7 @@ function calculateForSpan(
 
   // To correct this, check whether the user clicked at the start.
 
-  console.log('\nCHECK START\n\n')
-
-  // The document which contains the paragraph elements
-  const d = document.querySelectorAll('.document')[0]
-  // The bound of the document relative to the viewport
-  const cont = d.getBoundingClientRect()
-
-  // The left border of the line relative to the viewport
-  const leftBorder = cont.left + config.PARAGRAPH_PADDING
-  console.log('Left border within viewport:', leftBorder)
-
-  // Calculate the next position
-  const nextPos = incrementOffset(editor, pos)
-  if (nextPos !== null) {
-    // Check if user clicked at the start by comparing the bounds
-    // of the current position and next position
-    const currSpan = p.children[pos.sindex]
-    const currRect = Coords.getRectFromRange(
-      currSpan.childNodes[0],
-      pos.caret.offset
-    )
-    const nextSpan = p.children[nextPos.sindex]
-    const nextRect = Coords.getRectFromRange(
-      nextSpan.childNodes[0],
-      nextPos.offset
-    )
-
-    if (currRect.y !== nextRect.y) {
-      const currDist = Math.abs(currRect.x - clickX)
-      const nextDist = Math.abs(leftBorder - clickX)
-      if (nextDist < currDist) {
-        console.log('Snap to start')
-        pos.caret.x = config.PARAGRAPH_PADDING
-        pos.caret.y = nextRect.top - cont.top + d.scrollTop
-      }
-    }
-  }
+  checkOffsetRepeat(editor, pos, p, mouseX, mouseY)
 }
 
 function calculateForParagraph(
@@ -332,14 +171,79 @@ function calculateForParagraph(
   clickY: number
 ): void {
   console.log('Set caret for paragraph:', el)
+  console.log('focusOffset:', offset)
   // TODO: Clean up the implementation
+
+  // The document which contains the paragraphs
+  const d = document.querySelectorAll('.document')[0]
+  // The bounds of the document relative to the viewport
+  const cont = d.getBoundingClientRect()
+
+  // Mouse coordinates relative to the document
+  const mouseX = clickX - cont.left
+  const mouseY = clickY - cont.top + d.scrollTop
+
+  // The paragraph as an element
   const p = document.querySelectorAll('.paragraph')[pos.pindex] as HTMLElement
+  // The paragraph as an array
   const arr = editor.paragraphs[pos.pindex]
-  const res = fixToNearestSpan(p, arr, offset, clickX, clickY)
-  pos.caret.offset = res.caret.offset
-  pos.caret.x = res.caret.x
-  pos.caret.y = res.caret.y
-  pos.sindex = res.sindex
+
+  // Check for empty paragraph
+
+  if (arr[0].text.length === 0) {
+    console.log('Paragraph is empty')
+    pos.caret.x = config.PARAGRAPH_PADDING
+    pos.caret.y = p.offsetTop + config.ADJUST_Y
+    pos.caret.offset = 0
+    return
+  }
+
+  // Traverse the spans that contain the offset TODO:
+
+  /*const mid = Math.floor((arr.length - 1) / 2)
+  const midNode = arr[mid]
+  const midSpan = p.children[mid]
+  const midOffset = Math.floor(midNode.text.length / 2)
+  const [midX, midY] = Coords.getDocumentCoords(midSpan, midOffset)
+
+  const divider = midY - config.ADJUST_Y
+  console.log('divider:', divider)
+  const finalBound = divider + config.LINE_HEIGHT
+  console.log('final bound:', finalBound)
+
+  let increment = 1
+  let stop = arr.length
+  if (mouseY < finalBound) {
+    increment = -1
+    stop = -1
+  }*/
+
+  let shortestDistance = Number.MAX_VALUE
+  let candidate = 0
+  while (candidate < arr.length) {
+    const node = arr[candidate]
+    if (offset <= node.text.length) {
+      // Calculate distance from cursor position to candidate
+      const span = p.children[candidate]
+      const [newX, newY] = Coords.getDocumentCoords(span, offset)
+      const newPos: Position = {
+        caret: { offset, x: newX, y: newY },
+        pindex: pos.pindex,
+        sindex: candidate
+      }
+
+      // Check border case
+      const dist = checkOffsetRepeat(editor, newPos, p, mouseX, mouseY)
+      if (dist < shortestDistance) {
+        console.log('New best candidate:', candidate)
+        shortestDistance = dist
+        pos.caret.x = newPos.caret.x
+        pos.caret.y = newPos.caret.y
+        pos.sindex = candidate
+      } // TODO: break clause
+    }
+    candidate += 1
+  }
 }
 
 export function calculateCaretPosition(
@@ -356,6 +260,8 @@ export function calculateCaretPosition(
     pindex,
     sindex
   }
+
+  console.log('\n * * * CARET SETTING * * * \n\n')
 
   if (el.className === 'text-node') {
     calculateForSpan(pos, editor, el, offset, clickX, clickY)
