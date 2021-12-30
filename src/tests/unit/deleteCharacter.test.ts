6a27e11e1cd40e315e4550e9acb0abc526989ef5
Mia Mantila <mia.mantila@aalto.fi>
2021-12-30 16:12:03 +0200

    Add more test cases to deleteCharacter.test.ts

--- src/tests/unit/deleteCharacter.test.ts
+++ src/tests/unit/deleteCharacter.test.ts
@@ -97,6 +97,30 @@ describe('function deleteCharacter: Deletes a character correctly', () => {
     expect(example[status.pindex][status.sindex].text).toEqual('A')
   })
 
+  test('From a node with one character, between two nodes with the same style', () => {
+    const example = [
+      [
+        { style: normal(), text: 'A' },
+        { style: bold(), text: 'B' },
+        { style: normal(), text: 'C' }
+      ]
+    ]
+
+    const status = { pindex: 0, sindex: 1, offset: 1 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+    expect(example[status.pindex][status.sindex].text).toEqual('AC')
+  })
+
   test('From a node with one character, between two nodes with different styles', () => {
     const example = [
       [
@@ -122,16 +146,13 @@ describe('function deleteCharacter: Deletes a character correctly', () => {
     expect(example[status.pindex][status.sindex + 1].text).toEqual('C')
   })
 
-  test('From a node with one character, between two nodes with the same style', () => {
+  test('From an empty paragraph, with the previous paragraph ending in the same style', () => {
     const example = [
-      [
-        { style: normal(), text: 'A' },
-        { style: bold(), text: 'B' },
-        { style: normal(), text: 'C' }
-      ]
+      [{ style: normal(), text: 'A' }],
+      [{ style: normal(), text: '' }]
     ]
 
-    const status = { pindex: 0, sindex: 1, offset: 1 }
+    const status = { pindex: 1, sindex: 0, offset: 0 }
 
     deleteCharacter(example, status)
 
@@ -143,6 +164,245 @@ describe('function deleteCharacter: Deletes a character correctly', () => {
     // Check paragraphs
     expect(example.length).toEqual(1)
     expect(example[status.pindex].length).toEqual(1)
-    expect(example[status.pindex][status.sindex].text).toEqual('AC')
+    expect(example[status.pindex][status.sindex].text).toEqual('A')
+
+    // Check style
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(normal())
+  })
+
+  test('From an empty paragraph, with the previous paragraph ending in a different style', () => {
+    const example = [
+      [{ style: italic(), text: 'A' }],
+      [{ style: normal(), text: '' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+    expect(example[status.pindex][status.sindex].text).toEqual('A')
+
+    // Check style
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(italic())
+  })
+
+  test('From a paragraph with one node, with the previous paragraph ending in the same style', () => {
+    const example = [
+      [{ style: normal(), text: 'A' }],
+      [{ style: normal(), text: 'B' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+    expect(example[status.pindex][status.sindex].text).toEqual('AB')
+
+    // Check style
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(normal())
+  })
+
+  test('From a paragraph with two nodes, with the previous paragraph ending in the same style', () => {
+    const example = [
+      [{ style: normal(), text: 'A' }],
+      [
+        { style: normal(), text: 'B' },
+        { style: italic(), text: 'C' }
+      ]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(2)
+    expect(example[status.pindex][status.sindex].text).toEqual('AB')
+    expect(example[status.pindex][status.sindex + 1].text).toEqual('C')
+
+    // Check styles
+    expect(example[status.pindex][status.sindex].style).toEqual(normal())
+    expect(example[status.pindex][status.sindex + 1].style).toEqual(italic())
+  })
+
+  test('From a paragraph with one node, with the previous paragraph ending in a different style', () => {
+    const example = [
+      [{ style: italic(), text: 'A' }],
+      [{ style: normal(), text: 'B' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(2)
+    expect(example[status.pindex][status.sindex].text).toEqual('A')
+    expect(example[status.pindex][status.sindex + 1].text).toEqual('B')
+
+    // Check styles
+    expect(example[status.pindex][status.sindex].style).toEqual(italic())
+    expect(example[status.pindex][status.sindex + 1].style).toEqual(normal())
+  })
+
+  test('From a paragraph with two nodes, with the previous paragraph ending in a different style', () => {
+    const example = [
+      [{ style: bold(), text: 'A' }],
+      [
+        { style: normal(), text: 'B' },
+        { style: italic(), text: 'C' }
+      ]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(3)
+    expect(example[status.pindex][status.sindex].text).toEqual('A')
+    expect(example[status.pindex][status.sindex + 1].text).toEqual('B')
+    expect(example[status.pindex][status.sindex + 2].text).toEqual('C')
+
+    // Check styles
+    expect(example[status.pindex][status.sindex].style).toEqual(bold())
+    expect(example[status.pindex][status.sindex + 1].style).toEqual(normal())
+    expect(example[status.pindex][status.sindex + 2].style).toEqual(italic())
+  })
+
+  test('From an empty paragraph, with the previous paragraph being empty (same style)', () => {
+    const example = [
+      [{ style: normal(), text: '' }],
+      [{ style: normal(), text: '' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(0)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+
+    // Check style
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(normal())
+  })
+
+  test('From an empty paragraph, with the previous paragraph being empty (different style)', () => {
+    const example = [
+      [{ style: italic(), text: '' }],
+      [{ style: normal(), text: '' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(0)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+
+    // Check style
+    // * If both paragraphs are empty, favor the style of the original (deleted) node
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(normal())
+  })
+
+  test('From an empty paragraph, with the previous paragraph containing one node (same style)', () => {
+    const example = [
+      [{ style: normal(), text: 'A' }],
+      [{ style: normal(), text: '' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+    expect(example[status.pindex][status.sindex].text).toEqual('A')
+
+    // Check style
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(normal())
+  })
+
+  test('From an empty paragraph, with the previous paragraph containing one node (different style)', () => {
+    const example = [
+      [{ style: italic(), text: 'A' }],
+      [{ style: normal(), text: '' }]
+    ]
+
+    const status = { pindex: 1, sindex: 0, offset: 0 }
+
+    deleteCharacter(example, status)
+
+    // Check status
+    expect(status.pindex).toEqual(0)
+    expect(status.sindex).toEqual(0)
+    expect(status.offset).toEqual(1)
+
+    // Check paragraphs
+    expect(example.length).toEqual(1)
+    expect(example[status.pindex].length).toEqual(1)
+    expect(example[status.pindex][status.sindex].text).toEqual('A')
+
+    // Check style
+    const style = example[status.pindex][status.sindex].style
+    expect(style).toEqual(italic())
   })
 })
